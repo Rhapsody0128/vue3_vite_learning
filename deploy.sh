1cfa22414ba54db132b8ab0f1dc2e15677fe65a9
Rhapsody0128 <rhapsody0128@gmail.com>
2021-08-06 17:14:44 +0800

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+# 发生错误时终止
+set -e
+
+# 构建
+yarn build
+
+# 进入构建文件夹
+cd dist
+
+# 如果你要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果你要部署在 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+git push -f git@github.com:Rhapsody0128/vue3_vite_learning.git master:gh-pages
+
+cd -
