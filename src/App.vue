455f960466e877d99c6f0abb8e11e9a90b27eeff
Rhapsody0128 <rhapsody0128@gmail.com>
2021-08-06 16:47:04 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+
+img(src="./assets/logo.png")
+selfComponent(msg='selfComponent')
+p Store.Name = {{name}}
+router-link(to='/') Home | 
+router-link(to='/about')  About
+router-view
+
+</template>
+
+<script setup>
+import selfComponent from './components/selfComponent.vue'
+console.log('在script setup內的會在mounted之前被讀取');
+</script>
+<script>
+export default {
+  data(){
+    return{
+      name:'None'
+    }
+  },
+  mounted(){
+    console.log('在後面');
+    this.name = this.$store.state.name
+  }
+}
+</script>
+
+<style lang="stylus">
+#app 
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    name:'Gary',
+    Age:27
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
